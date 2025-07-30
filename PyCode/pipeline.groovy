589dae4b41ee9bae9b9a861796cb61b24e6461d1
z-aditya-pradhan <adityap@zenarate.com>
2025-07-31 00:50:03 +0530

    Added changes w.r.t Authoring and pipeline

--- PyCode/pipeline.groovy
+++ PyCode/pipeline.groovy
@@ -1,23 +1,23 @@
 pipeline{
     agent any
-//     environment {
-//         ARM_CLIENT_ID = credentials('ARM_CLIENT_ID')
-//         ARM_CLIENT_SECRET = credentials('ARM_CLIENT_SECRET')
-//         ARM_TENANT_ID = credentials('ARM_TENANT_ID')
-//     }
+    environment {
+        ARM_CLIENT_ID = credentials('ARM_CLIENT_ID')
+        ARM_CLIENT_SECRET = credentials('ARM_CLIENT_SECRET')
+        ARM_TENANT_ID = credentials('ARM_TENANT_ID')
+    }
     stages{
 
         stage ('Nginx Configuration'){
             steps{
                 dir("PyCode"){
                     sh '''
-                        python3 -m venv venvbs1
-                        . venvbs1/bin/activate
+                        python3 -m venv venvbs2
+                        . venvbs2/bin/activate
                         pip install --upgrade pip
                         pip install -r requirements.txt
 
                         # Safely run Python script with proper quoting
-                        python3 nginx_config.py --env $env --url "$brand_url" --exectype $execution_type
+                        python3 LUISAuthoring.py --subscription_id $subscription_id
                     '''
                 }
             }
